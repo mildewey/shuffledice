76dca154bfa44f728ab6ff3c0efe3772934bcb72
Mark Dewey <mildewey@gmail.com>
2019-01-18 13:41:24 -0700

    Adds basic logging and config.

--- shuffledice/main.py
+++ shuffledice/main.py
@@ -1,6 +1,15 @@
 from aiohttp import web
 from routes import setup_routes
 
+import config
+config.init()
+
+import logging
+logger = logging.getLogger(__name__)
+
+logger.info('Initializing app')
 app = web.Application()
+logger.info('Setting up routes')
 setup_routes(app)
+logger.info('Starting web server')
 web.run_app(app)
